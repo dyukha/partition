bbf64b6c89809fffdef1742672861e679346de87
dyukha <dimonbv@gmail.com>
2018-05-08 11:59:31 -0400

    Bat files

---
+++ compile_win.cmd
@@ -0,0 +1,2 @@
+mkdir output
+g++ -Wall -Wno-sign-compare -Wextra -std=c++17 src/Runner.cpp -O2 -o output/Runner.exe 
---
+++ run.cmd
@@ -0,0 +1,6 @@
+@echo off
+call compile_win.cmd
+set dir=output/%1
+echo %dir%
+mkdir %dir%
+output\Runner.exe %dir% 2>&1 | tee %dir%/err
